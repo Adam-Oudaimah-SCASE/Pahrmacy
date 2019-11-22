3825ca0dbe0748a86fb8476a6b1487d9b9f542cd
Adam Oudaimah <adam.oudaimah@lit-co.net>
2019-11-23 00:53:15 +0200

    Fix migration error: Syntax error or access violation: 1071 Specified key was too long.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Fix Error when running migrations
+        // TODO: Search how to fix this problem
+        Schema::defaultStringLength(191);
     }
 }
