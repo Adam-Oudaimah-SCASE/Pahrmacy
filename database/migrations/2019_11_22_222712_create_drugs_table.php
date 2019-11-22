ee485d4bea2b9f6ca505a8725ea261c2d6ba8996
Adam Oudaimah <adam.oudaimah@lit-co.net>
2019-11-23 01:04:30 +0200

    Initial database structure (bills excluded).

---
+++ database/migrations/2019_11_22_222712_create_drugs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDrugsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drugs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_english');
+            $table->string('name_arabic')->nullable();
+            $table->string('chemical_composition');
+            $table->string('content')->nullable();
+            $table->string('shape');
+            $table->string('manufacturer')->nullable();
+            $table->string('volume_unit')->nullable();
+            $table->integer('net_price');
+            $table->integer('sell_price');
+            $table->integer('lic_palte')->nullable();
+            $table->string('local_barcode');
+            $table->string('global_barcode');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('drug_categories')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drugs');
+    }
+}
