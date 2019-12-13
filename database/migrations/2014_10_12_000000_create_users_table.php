95420e4785ceb1dc1229524bc0788c6d537d2964
Adam Oudaimah <adam.oudaimah@lit-co.net>
2019-12-13 17:03:35 +0200

    Add required attributes to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('phone');
+            $table->integer('salary');
+            $table->string('notes')->nullable();
+            $table->string('national_id')->nullable();
+            $table->string('education')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
