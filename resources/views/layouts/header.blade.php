c593cfe8d6ca8f4a4bf83f20f1fed90337ae6796
Hasnaa Turk <alhasnaa.tu@gmail.com>
2020-01-27 12:59:02 +0200

    added sidebar's data footer is now in arabic

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,7 +11,7 @@
 
       <div class="top-menu">
         <ul class="nav pull-right top-menu">
-          <li><a class="logout" href="login.html">Logout</a></li>
+          <li><a class="logout" href="login.html">تسجيل الخروج</a></li>
         </ul>
       </div>
     </header>
@@ -31,11 +31,105 @@
           </a></p>
           <h5 class="centered">NODES PHARMA</h5>
           <li class="mt">
-            <a href="index.html">
-              <i class="fa fa-dashboard"></i>
-              <span>Dashboard</span>
-              </a>
+
+            <li class="sub-menu">
+              <a href="javascript:;">
+              <i class="fa fa-plus-square"></i>
+              <span>الصيدلية</span>
+            </a>
+            <ul class="sub">
+              <li><a href="#">معلومات الصيدلية</a></li>
+              <li><a href="#">الموظفين</a></li>
+              <li><a href="#">الحالة المالية</a></li>
+            <li><a href="#">	 </a></li>
+            </ul>
           </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+            <i class="fa fa-flask"></i>
+            <span>الأدوية</span>
+          </a>
+          <ul class="sub">
+            <li><a href="#">	عرض الادوية </a></li>
+            <li><a href="#">	الأقسام</a></li>
+            <li><a href="#">	الاشكال الدوائية </a></li>
+            <li><a href="#">	تقرير مبيعات الأقسام </a></li>
+            <li><a href="#">	تقرير الأدوية الحالية </a></li>
+            <li><a href="#">	تقرير كميات الادوية المنتهية الصلاحية 	 </a></li>
+            <li><a href="#">	تقرير الأدوية منتهية الصلاحية 	 </a></li>
+          </ul>
+        </li>
+
+        <li class="sub-menu">
+          <a href="javascript:;">
+          <i class="fa fa-building-o"></i>
+          <span>الشركات</span>
+        </a>
+        <ul class="sub">
+
+        <li><a href="#">	الشركات المنتجة 	 </a></li>
+          <li><a href="#">	تقرير أدوية الشركة	 </a></li>
+        </ul>
+      </li>
+
+      <li class="sub-menu">
+        <a href="javascript:;">
+        <i class="fa fa-archive"></i>
+        <span>المستودعات</span>
+      </a>
+      <ul class="sub">
+        <li><a href="#">	المستودعات الموجودة 	 </a></li>
+      <li><a href="#">	تقرير مستودعات 	 </a></li>
+      </ul>
+    </li>
+
+
+    <li class="sub-menu">
+      <a href="javascript:;">
+      <i class="fa fa-calculator"></i>
+      <span>الفواتير</span>
+    </a>
+    <ul class="sub">
+
+    <li><a href="#">	فاتورة مبيعات 	 </a></li>
+    <li><a href="#">	فاتورة مشتريات 	 </a></li>
+    <li><a href="#">	استلام فاتورة مبيعات 	 </a></li>
+    <li><a href="#">	فاتورة مرتجع 	 </a></li>
+    <li><a href="#">		فاتورة تالف  </a></li>
+    <li><a href="#">	تقرير يومية المبيعات 	 </a></li>
+    <li><a href="#">	تقرير أجمالي مبيعات خلال فترة 	 </a></li>
+    <li><a href="#">	تقرير اجمالي مشتريات خلال فترة 	 </a></li>
+    <li><a href="#">	تقرير الأرباح خلال فترة 	 </a></li>
+
+    </ul>
+  </li>
+  <li class="sub-menu">
+    <a href="javascript:;">
+    <i class="fa fa-briefcase"></i>
+    <span>الصندوق</span>
+  </a>
+  <ul class="sub">
+
+  <li><a href="#">	مبيعات خلال فترة معينة 	 </a></li>
+  <li><a href="#">	مدفوعات خلال فترة معينة 	 </a></li>
+  <li><a href="#">		تقرير شهري </a></li>
+  <li><a href="#">	تقرير يومي 	 </a></li>
+  <li><a href="#">	تقرير سنوي   </a></li>
+
+  </ul>
+  <li>
+    <a href="#">
+      <i class="fa fa-gears"></i>
+      <span>الدعم التقني </span>
+      </a>
+  </li>
+  <li>
+    <a href="#">
+      <i class="fa fa-group"></i>
+      <span>من نحن </span>
+      </a>
+  </li>
+</li>
           <li class="sub-menu">
             <a href="javascript:;">
               <i class="fa fa-desktop"></i>
@@ -48,97 +142,7 @@
               <li><a href="font_awesome.html">Font Awesome</a></li>
             </ul>
           </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-cogs"></i>
-              <span>Components</span>
-              </a>
-            <ul class="sub">
-              <li><a href="grids.html">Grids</a></li>
-              <li><a href="calendar.html">Calendar</a></li>
-              <li><a href="gallery.html">Gallery</a></li>
-              <li><a href="todo_list.html">Todo List</a></li>
-              <li><a href="dropzone.html">Dropzone File Upload</a></li>
-              <li><a href="inline_editor.html">Inline Editor</a></li>
-              <li><a href="file_upload.html">Multiple File Upload</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-book"></i>
-              <span>Extra Pages</span>
-              </a>
-            <ul class="sub">
-              <li><a href="blank.html">Blank Page</a></li>
-              <li><a href="login.html">Login</a></li>
-              <li><a href="lock_screen.html">Lock Screen</a></li>
-              <li><a href="profile.html">Profile</a></li>
-              <li><a href="invoice.html">Invoice</a></li>
-              <li><a href="pricing_table.html">Pricing Table</a></li>
-              <li><a href="faq.html">FAQ</a></li>
-              <li><a href="404.html">404 Error</a></li>
-              <li><a href="500.html">500 Error</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a class="active" href="javascript:;">
-              <i class="fa fa-tasks"></i>
-              <span>Forms</span>
-              </a>
-            <ul class="sub">
-              <li class="active"><a href="form_component.html">Form Components</a></li>
-              <li><a href="advanced_form_components.html">Advanced Components</a></li>
-              <li><a href="form_validation.html">Form Validation</a></li>
-              <li><a href="contactform.html">Contact Form</a></li>
-              <li><a href="contactform.html">Contact Form</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-th"></i>
-              <span>Data Tables</span>
-              </a>
-            <ul class="sub">
-              <li><a href="basic_table.html">Basic Table</a></li>
-              <li><a href="responsive_table.html">Responsive Table</a></li>
-              <li><a href="advanced_table.html">Advanced Table</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="inbox.html">
-              <i class="fa fa-envelope"></i>
-              <span>Mail </span>
-              <span class="label label-theme pull-right mail-info">2</span>
-              </a>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class=" fa fa-bar-chart-o"></i>
-              <span>Charts</span>
-              </a>
-            <ul class="sub">
-              <li><a href="morris.html">Morris</a></li>
-              <li><a href="chartjs.html">Chartjs</a></li>
-              <li><a href="flot_chart.html">Flot Charts</a></li>
-              <li><a href="xchart.html">xChart</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-comments-o"></i>
-              <span>Chat Room</span>
-              </a>
-            <ul class="sub">
-              <li><a href="lobby.html">Lobby</a></li>
-              <li><a href="chat_room.html"> Chat Room</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="google_maps.html">
-              <i class="fa fa-map-marker"></i>
-              <span>Google Maps </span>
-              </a>
-          </li>
+          
         </ul>
         <!-- sidebar menu end-->
       </div>
