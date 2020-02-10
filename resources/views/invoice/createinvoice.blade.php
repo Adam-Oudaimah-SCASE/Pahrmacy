8f346f539d0d6a907683c2071ec2357b888f5dff
Hasnaa Turk <alhasnaa.tu@gmail.com>
2020-02-10 14:11:38 +0200

    edited createinvoice view

--- resources/views/invoice/createinvoice.blade.php
+++ resources/views/invoice/createinvoice.blade.php
@@ -50,80 +50,99 @@
               <!-- /invoice-body -->
             </div>
             <!-- /col-lg-10 -->
-            <table class="table">
+
+              <form class="form-horizontal style-form" action="" method="POST">
+                  @csrf
+              <table class="table">
               <thead>
                 <tr>
                   <th class="text-left">اسم الدواء بالعربي </th>
-                  <th class="text-left">اسم الدواء بالانكليزي</th>
                   <th class="text-left">تاريخ انتهاء الصلاحية</th>
-                  <th style="width:200px" class="text-left">السعر الصافي للظرف</th>
-                  <th style="width:200px" class="text-left">السعر الصافي للعلبة</th>
-                  <th style="width:200px" class="text-left">سعر المبيع للظرف</th>
-                  <th style="width:200px" class="text-left">سعر المبيع للعلبة</th>
-                  <th style="width:100px" class="text-center">عدد العلب</th>
-                  <th style="width:100px" class="text-center">عدد الظروف</th>
-                  <th style="width:100px" class="text-left">السعر المعدل للظرف</th>
-                  <th style="width:100px" class="text-left">السعر المعدل للعلبة</th>
+                  <th style="width:150px" class="text-center">عدد العلب</th>
+                  <th style="width:150px" class="text-center">عدد الظروف</th>
+                  <th style="width:150px" class="text-left">السعر المعدل للظرف</th>
+                  <th style="width:150px" class="text-left">السعر المعدل للعلبة</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>Flat Pack Heritage</td>
-                  <td>Flat Pack Heritage</td>
-                  <td class="text-center">11-12-2020</td>
-                  <td class="text-center">221</td>
-                  <td class="text-center">221</td>
-                  <td class="text-center">221</td>
-                  <td class="text-center">221</td>
-                  <form class="form-horizontal style-form" action="" method="POST">
-                  @csrf
-                      <div class="form-group" dir="rtl">
+                  <td class="text-left">11-12-2020</td>
+
                       <td class="text-center">  <input type="text" class="form-control" name="name"></td>
                       <td class="text-center">  <input type="text" class="form-control" name="name"></td>
                       <td class="text-center">  <input type="text" class="form-control" name="name"></td>
                       <td class="text-center">  <input type="text" class="form-control" name="name"></td>
-                      </div>
-                  </form>
-              </tr>
-              <tr>
-                <td>Flat Pack Heritage</td>
-                <td>Flat Pack Heritage</td>
-                <td class="text-center">11-12-2020</td>
-                <td class="text-center">221</td>
-                <td class="text-center">221</td>
-                <td class="text-center">221</td>
-                <td class="text-center">221</td>
-                <form class="form-horizontal style-form" action="" method="POST">
-                @csrf
-                    <div class="form-group" dir="rtl">
-                    <td class="text-center">  <input type="text" class="form-control" name="name"></td>
-                    <td class="text-center">  <input type="text" class="form-control" name="name"></td>
-                    <td class="text-center">  <input type="text" class="form-control" name="name"></td>
-                    <td class="text-center">  <input type="text" class="form-control" name="name"></td>
-                    </div>
-                </form>
-            </tr>
+
+                  </tr>
+
+                  <tr>
+                    <td>Flat Pack Heritage</td>
+                    <td class="text-left">11-12-2020</td>
+
+                        <td class="text-center">  <input type="text" class="form-control" name="name"></td>
+                        <td class="text-center">  <input type="text" class="form-control" name="name"></td>
+                        <td class="text-center">  <input type="text" class="form-control" name="name"></td>
+                        <td class="text-center">  <input type="text" class="form-control" name="name"></td>
+
+                    </tr>
            </tbody>
+
         </table>
             <hr style="border-color:#ddd;">
             <br>
            <table>
            <tbody>
             <tr>
-            <form class="form-horizontal style-form" action="" method="POST">
-            @csrf
-                <div class="form-group" dir="rtl">
+
+
                 <td class="text-center">  <label class="col-sm-2 col-sm-2 control-label">الخصم</label>  </td>
                 <td style="width:300px;"class="text-center">  <input type="text" class="form-control" name="name"></td>
                 <td style="width:300px;" class="text-center"> <label class=" control-label">سبب الخصم</label>  </td>
-                <td style="width:600px;" class="text-center"> <input type="text-area" class="form-control" name="name"> </td>
+                <td style="width:600px;" class="text-center">
+                  <select class="form-control">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select> </td>
 
-                <td class="text-center">    <button type="submit" class="btn btn-theme">حفظ</button></td>
-                </div>
-            </form>
+                <td class="text-center">    <button type="button" class="btn btn-theme" data-toggle="modal" data-target="#myModal">حفظ</button></td>
+                <!-- Modal -->
+                 <div class="modal fade" id="myModal" role="dialog">
+                   <div class="modal-dialog modal-lg">
+                     <div class="modal-content">
+                       <div class="modal-header">
+                         <button type="button" class="close" data-dismiss="modal">&times;</button>
+                         <h4 class="modal-title">حفظ الفاتورة</h4>
+                       </div>
+                       <div class="modal-body">
+                         <div class="form-group mt">
+                           <label class="col-sm-2 col-lg-2 control-label">السعر الصافي</label>
+                           <div class="col-lg-10">
+                           <input type="text" class="form-control " name="name">
+                           </div>
+                         </div>
+                      <div class="form-group">
+                        <label class="col-sm-2 col-sm-2 control-label">سعر المبيع</label>
+                        <div class="col-lg-10">
+                        <input type="text" class="form-control " name="name">
+                        </div>
+                      </div>
+                       </div>
+                       <div class="modal-footer">
+                          <button type="submit" class="btn btn-default" >حفظ</button>
+                         <button type="button" class="btn btn-default" data-dismiss="modal">إغلاق</button>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+
           </tbody>
         </tr>
       </table>
+        </form>
             <br>
             <br>
           </div>
